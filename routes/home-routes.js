001638f0f2478b398432de9bd1023ee8a0cf2ca0
Denny Doan <denny-333@hotmail.com>
2023-02-12 21:54:18 -0500

    Added home-routes

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const router = express.Router();
-const {getAllResources} = require('../db/queries/resources');
-
 const client = require('../db/connect');
+
 
 // loads and renders home page
 router.get('/', (req, res) => {
-  getAllResources()
-  .then(() => {
-    return res.render('/home');
+  return resources.getAllResources()
+  .then((resources) => {
+    return res.json(resources);
   })
 });
+
+// loads and renders home page
+router.get('/home', (req, res) => {
+  return res.redirect('/')
+});
+
 
 module.exports = router;
