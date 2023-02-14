d299a528b68db31884eff47055f00e2e3142c3ce
Jason Divinagracia <jmarkd04@gmail.com>
2023-02-13 20:24:33 -0500

    Resources now pop up on screen

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,48 +1,43 @@
-const renderResources = function(resources) {
-  $("#resources-container").empty();
-  for (const resource of resources) {
-    const $resource = createresourceElement(resource);
-    $('#resources-container').prepend($resource);
-  }
-};
-
-// const escape = function (str) {
-//   let div = document.createElement("div");
-//   div.appendChild(document.createTextNode(str));
-//   return div.innerHTML;
-// };
-
-// const createResourceElement = resourceObject => {
-//   let $resource =
-//   `
-//     <article class="resource">
-//       <header>
-//         <div class="user-resource">
-//           <img src="${resourceObject.user.avatars}">
-//           ${resourceObject.user.name}
-//         </div>
-//         <span class="user-tag">${resourceObject.user.handle}</span>
-//       </header>
-//       <p>${escape(resourceObject.content.text)}</p>
-//       <footer>
-//         <span>${timeago.format(resourceObject.created_at)}</span>
-//         <div>
-//           <i class="fa-solid fa-flag"></i>
-//           <i class="fa-solid fa-share"></i>
-//           <i class="fa-solid fa-heart"></i>
-//         </div>
-//       </footer>
-//     </article>
-//   `;
-//   return $resource;
-// };
-
 $(() => {
+  const renderResources = function (resources) {
+    $("#resources-container").empty();
+    for (const resource of resources) {
+      const $resource = createResourceElement(resource);
+      $("#resources-container").prepend($resource);
+    }
+  };
+
+  // const escape = function (str) {
+  //   let div = document.createElement("div");
+  //   div.appendChild(document.createTextNode(str));
+  //   return div.innerHTML;
+  // };
+
+  const createResourceElement = (resourceObject) => {
+    let $resource = `
+  <div class="resource-containers">
+  <div id="col s12 m6">
+    <div class="res-cards">
+      <div class="res-image">
+        <img src="${resourceObject.media_url}">
+        <span class="res-title">${resourceObject.title}</span>
+        <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+      </div>
+      <div class="res-content">
+        <p>${resourceObject.description}</p>
+      </div>
+    </div>
+  </div>
+  </div>
+  `;
+    return $resource;
+  };
+
   const loadResources = () => {
     $.get("http://localhost:8080/resources/all", (data) => {
       renderResources(data);
-    })
-  }
+    });
+  };
 
   loadResources();
 });
