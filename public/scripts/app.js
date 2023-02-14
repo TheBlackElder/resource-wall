8be83f5b8ea76f550d0a2b235c21a1164c6dcb28
Jason Divinagracia <jmarkd04@gmail.com>
2023-02-14 13:04:44 -0500

    modified app.js, resources.scss, category-tabs.ejs, resources.ejs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -15,23 +15,32 @@ $(() => {
 
   const createResourceElement = (resourceObject) => {
     let $resource = `
-  <div class="resource-containers">
-  <div id="col s12 m6">
-    <div class="res-cards">
-      <div class="res-image">
-        <img src="${resourceObject.media_url}">
-        <span class="res-title">${resourceObject.title}</span>
-        <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+      <div class="resource">
+          <img src="${resourceObject.media_url}">
       </div>
-      <div class="res-content">
-        <p>${resourceObject.description}</p>
-      </div>
-    </div>
-  </div>
-  </div>
   `;
     return $resource;
   };
+
+  // const createResourceElement = (resourceObject) => {
+  //   let $resource = `
+  // <div class="resource-containers">
+  // <div id="col s12 m6">
+  //   <div class="res-cards">
+  //     <div class="res-image">
+  //       <img src="${resourceObject.media_url}">
+  //       <span class="res-title">${resourceObject.title}</span>
+  //       <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+  //     </div>
+  //     <div class="res-content">
+  //       <p>${resourceObject.description}</p>
+  //     </div>
+  //   </div>
+  // </div>
+  // </div>
+  // `;
+  //   return $resource;
+  // };
 
   const loadResources = () => {
     $.get("http://localhost:8080/resources/all", (data) => {
