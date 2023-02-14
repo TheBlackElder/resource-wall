f996448e9450d1dbfbeb3ddafb4ede915bbd2891
Jason Divinagracia <jmarkd04@gmail.com>
2023-02-13 19:35:56 -0500

    Created resource routes and updated app.js in scripts

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,48 @@
-// Client facing scripts here
+const renderResources = function(resources) {
+  $("#resources-container").empty();
+  for (const resource of resources) {
+    const $resource = createresourceElement(resource);
+    $('#resources-container').prepend($resource);
+  }
+};
+
+// const escape = function (str) {
+//   let div = document.createElement("div");
+//   div.appendChild(document.createTextNode(str));
+//   return div.innerHTML;
+// };
+
+// const createResourceElement = resourceObject => {
+//   let $resource =
+//   `
+//     <article class="resource">
+//       <header>
+//         <div class="user-resource">
+//           <img src="${resourceObject.user.avatars}">
+//           ${resourceObject.user.name}
+//         </div>
+//         <span class="user-tag">${resourceObject.user.handle}</span>
+//       </header>
+//       <p>${escape(resourceObject.content.text)}</p>
+//       <footer>
+//         <span>${timeago.format(resourceObject.created_at)}</span>
+//         <div>
+//           <i class="fa-solid fa-flag"></i>
+//           <i class="fa-solid fa-share"></i>
+//           <i class="fa-solid fa-heart"></i>
+//         </div>
+//       </footer>
+//     </article>
+//   `;
+//   return $resource;
+// };
+
+$(() => {
+  const loadResources = () => {
+    $.get("http://localhost:8080/resources/all", (data) => {
+      renderResources(data);
+    })
+  }
+
+  loadResources();
+});
