27f58ea9ea31c146149b3c8e25f28e59b553851e
Denny Doan <denny-333@hotmail.com>
2023-02-14 17:20:14 -0500

    Testing search

--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -1,32 +1,32 @@
-const renderSearch = (searchResults) => {
-  const renderResources = function () {
-    $("#resources-wrapper").empty();
-    for (const resource of searchResults) {
-      const $resource = createResourceElement(resource);
-      $("#resources-wrapper").append($resource);
-    }
-  };
+// const renderSearch = (searchResults) => {
+//   const renderResources = function () {
+//     $("#resources-wrapper").empty();
+//     for (const resource of searchResults) {
+//       const $resource = createResourceElement(resource);
+//       $("#resources-wrapper").append($resource);
+//     }
+//   };
 
-  const createResourceElement = (resourceObject) => {
-    let $resource = `
-      <div class="resource-containers">
-        <div id="col s12 m4 l2">
-          <div class="res-cards">
-            <div class="res-image">
-              <img src="${resourceObject.media_url}">
-              <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="fa-solid fa-heart"></i></a>
-            </div>
-          </div>
-        </div>
-      </div>
-    `;
-    return $resource;
-  };
+//   const createResourceElement = (resourceObject) => {
+//     let $resource = `
+//       <div class="resource-containers">
+//         <div id="col s12 m4 l2">
+//           <div class="res-cards">
+//             <div class="res-image">
+//               <img src="${resourceObject.media_url}">
+//               <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="fa-solid fa-heart"></i></a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     `;
+//     return $resource;
+//   };
 
-  renderResources();
-};
+//   renderResources();
+// };
 
-module.exports = { renderSearch };
+// module.exports = { renderSearch };
 
 
 
